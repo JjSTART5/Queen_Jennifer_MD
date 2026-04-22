fa943c0d98d065526b079296cc7c2476b9456867
JjSTART5 <jeremiekema739@gmail.com>
2026-04-22 17:08:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,9 +14,9 @@ const { ownername, botname, footer, ig, region, myweb, packname, author, session
 
 //customize settings\\
 global.APIKeys = {'https://zenzapis.xyz': YOUR_API_KEY } //credit goes to sanzy yt, if api key expire, buy it and put it here?\\ https://zenzapis.xyz
-global.owner = ['94761539856'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
-global.premium = ['94761539856'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
-global.ownernomer = '94761539856' // Add Your number Eg: ['YOUR NUMBER']
+global.owner = ['242056632989'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
+global.premium = ['242056632689'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
+global.ownernomer = '056632689' // Add Your number Eg: ['YOUR NUMBER']
 // we got your name in your whatsapp
 
 
@@ -25,8 +25,8 @@ global.ownernomer = '94761539856' // Add Your number Eg: ['YOUR NUMBER']
 
 //Don't edit enithing\\
 global.APIs = APIs
-global.ownername = ownername
-global.botname = botname
+global.ownername = JjSTART
+global.botname = spidebot
 global.footer = footer
 global.ig = ig
 global.region = region
